14c63dc4f6f574451aa7ad6a207190d728146461
Mohit Sharma <mohitsharma@walkover.in>
2022-01-14 17:58:17 +0530

    add index formgroup and validation

--- src/app/user/pages/index/index.component.ts
+++ src/app/user/pages/index/index.component.ts
@@ -1,39 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import{FormBuilder , Validators, FormGroup,AbstractControl} from '@angular/forms'
+import { FormBuilder, Validators, FormGroup, AbstractControl, FormControl } from '@angular/forms'
 
+import { MEDIUM_PASSWORD_REGEX, ONLY_DIGIT } from '../../../utils/RegexConstant';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
-  loginForm:FormGroup;
-  checkform:boolean=false;
-
+  public loginForm: FormGroup;
+  public checkform: boolean = false;
 
   constructor(
-    private _fb:FormBuilder
-  ) 
-  {this.loginForm=this._fb.group({
-
-    "mobile":["",Validators.required],
-    "password":["",Validators.required]
-
-  }) }
+    private _fb: FormBuilder
+  ) {
+    this.logInFormInit();
+  }
 
   ngOnInit(): void {
   }
-  onSubmit(){
-    if(this.loginForm.invalid)
-    {
-      this.checkform= true;
-      return;
-    }
-    else
-    {
-      console.log(this.loginForm.value);
+
+  public logInFormInit() {
+    this.loginForm = new FormGroup({
+      mobile: new FormControl('', [Validators.required, Validators.maxLength(10), Validators.minLength(10)]),
+      password: new FormControl('', [Validators.required, Validators.pattern(MEDIUM_PASSWORD_REGEX)])
+    })
+  }
+
+  public onlyDigit(e: KeyboardEvent): void {
+    const inputChar = String.fromCharCode(e.charCode);
+    if (e.keyCode !== 8 && !ONLY_DIGIT.test(inputChar)) {
+        e.preventDefault();
     }
   }
 
+  onSubmit() {
+    console.log('innn submit', this.loginForm)
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {  ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { IndexComponent } from './pages/index/index.component';
-
-
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
 @NgModule({
   declarations: [
     UserComponent,
@@ -15,8 +18,10 @@ import { IndexComponent } from './pages/index/index.component';
     CommonModule,
     UserRoutingModule,
     ReactiveFormsModule,
-    FormsModule 
-   
+    FormsModule,
+    InputTextModule,
+    PasswordModule,
+    DividerModule
   ]
 })
 export class UserModule { }
