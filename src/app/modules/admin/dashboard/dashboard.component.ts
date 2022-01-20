be7eb3a2bc7e09fd334e4926d42282fc46a1bcdb
Mohit Sharma <mohitsharma@walkover.in>
2022-01-21 00:41:54 +0530

    graph changes as per mock up, scss added

--- src/app/modules/admin/dashboard/dashboard.component.ts
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -12,7 +12,18 @@ export class DashboardComponent implements OnInit {
   public dateFormat: string = 'dd M yy';
   public maxDateValue: Date = new Date();
   public chartData: any;
-  public chartOptions: any;
+  public graphLabelDetail: any[] = []
+  public chartOptions: any = {
+    maintainAspectRatio: false,
+    responsive: true,
+    cutout: 60,
+    elements: {
+      arc: {
+        borderWidth: 0
+      }
+    },
+  };
+  public graphColor = [ "#21d59b", "#0058ff", "#f0142f", "#f99600", "#ffc700"]
 
   constructor() {
     
@@ -21,6 +32,7 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.rangeDates = [new Date(), new Date()];
     this.setChartData();
+    this.setGraphLabelDetail();
   }
 
   public dateChange(event: any): void {
@@ -32,26 +44,24 @@ export class DashboardComponent implements OnInit {
 
   public setChartData() {
     this.chartData = {
-      labels: ["Accepted", "Pending", "Cancelled", "Delivered", "Shipped"],
+      // labels: ["Accepted", "Pending", "Cancelled", "Delivered", "Shipped"],
       datasets: [
         {
           data: [55, 63, 20, 24, 25],
-          backgroundColor: [
-            "#21d59b",
-            "#0058ff",
-            "#f0142f",
-            "#f99600",
-            "#ffc700"
-          ],
-          hoverBackgroundColor: [
-            "#21d59b",
-            "#0058ff",
-            "#f0142f",
-            "#f99600",
-            "#ffc700"
-          ]
-      }
+          backgroundColor: this.graphColor,
+          hoverBackgroundColor: this.graphColor
+        }
       ]
     }
+  }
+
+  public setGraphLabelDetail() {
+    this.graphLabelDetail = [
+      { status: 'Pending', first: '600', second: '23242424' },
+      { status: 'Accepted', first: '27', second: '234234234' },
+      { status: 'Shipped', first: '22', second: '24234234' },
+      { status: 'Delivered', first: '2505', second: '23424234' },
+      { status: 'Cancelled', first: '720', second: '234234234' },      
+    ]
   }
 }
