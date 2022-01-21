b3072ae438bf028bbd63870661bf21f5b15e685b
Mohit Sharma <mohitsharma@walkover.in>
2022-01-21 23:21:25 +0530

    hide chart lagends

--- src/app/modules/admin/dashboard/dashboard.component.ts
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -6,13 +6,15 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  // how to hilight doughunt chart section on click of button
+  // https://stackoverflow.com/questions/31033248/highlight-area-programmatically-chart-js
   @ViewChild('dashboardCalendar') dashboardCalendar: any;
   public rangeDates: Date[];
   public dateFormat: string = 'dd M yy';
   public maxDateValue: Date = new Date();
   public chartData: any;
   public graphLabelDetail: any[] = []
+  public graphColor = [ "#21d59b", "#0058ff", "#f0142f", "#f99600", "#ffc700"]
   public chartOptions: any = {
     maintainAspectRatio: false,
     responsive: true,
@@ -22,36 +24,39 @@ export class DashboardComponent implements OnInit {
         borderWidth: 0
       }
     },
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
   };
-  public graphColor = [ "#21d59b", "#0058ff", "#f0142f", "#f99600", "#ffc700"]
 
   constructor() {
     
   }
 
   ngOnInit(): void {
-    this.rangeDates = [new Date(), new Date()];
+    this.rangeDates = [new Date(this.maxDateValue.getFullYear(), this.maxDateValue.getMonth() - 3, 1), new Date()];
     this.setChartData();
     this.setGraphLabelDetail();
   }
 
   public dateChange(event: any): void {
-    // console.log('this.rangeDates', this.rangeDates)
-    if (this.rangeDates[1]) { // If second date is selected
+    if (this.rangeDates[1]) {
       this.dashboardCalendar.overlayVisible=false;
     }
   }
 
   public setChartData() {
     this.chartData = {
-      // labels: ["Accepted", "Pending", "Cancelled", "Delivered", "Shipped"],
+      labels: ["Accepted", "Pending", "Cancelled", "Delivered", "Shipped"],
       datasets: [
         {
           data: [55, 63, 20, 24, 25],
           backgroundColor: this.graphColor,
           hoverBackgroundColor: this.graphColor
         }
-      ]
+      ],
     }
   }
 
