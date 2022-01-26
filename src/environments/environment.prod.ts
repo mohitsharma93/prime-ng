1b8dc6b8955b7ce57324638059995cba722dce26
Mohit Sharma <mohitsharma@walkover.in>
2022-01-27 00:20:30 +0530

    api proxy add on prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: 'http://192.168.1.135:5252/'
+  API_ENDPOINT: 'http://192.168.1.135:5252/',
+  API_ENDPOINT_PROXY: 'api/v1/sellerDashboard/ShopOverview'
 };
