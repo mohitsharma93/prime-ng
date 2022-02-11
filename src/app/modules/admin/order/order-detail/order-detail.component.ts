22cd9fdadfffeda645ba40d38430c66bf2c46bf9
Mohit Sharma <mohitsharma@walkover.in>
2022-02-11 19:05:54 +0530

    comment redirection

--- src/app/modules/admin/order/order-detail/order-detail.component.ts
+++ src/app/modules/admin/order/order-detail/order-detail.component.ts
@@ -97,7 +97,7 @@ export class OrderDetailComponent extends BaseComponent implements OnInit {
 
   public redirectToDetail(id: string): void {
     if (id) {
-      this.router.navigate(['/admin', 'order-detail', id]);
+      // this.router.navigate(['/admin', 'order-detail', id]);
     }
   }
 
